b5467995a68322e15f2b7bb430d7763f1ca179c9
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2025-01-01 04:32:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = '0123'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0802@localhost:5432/POS'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://capstone_hoxl_user:dbBJycHv1ZHLYhYF46pgQ4VhDU1uRuCx@dpg-ctq58g1opnds73f3sp70-a/capstone_hoxl'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
